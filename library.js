dad352a6341f3a3598df3e36524e47134f482048
Jon Garrity <jonnygarrity@gmail.com>
2021-07-12 19:54:04 -0500

    Added read status button toggling. Added book delete button.

--- library.js
+++ library.js
@@ -1,17 +1,17 @@
 // generate test books
 const testBook1 = {
     title: "Gulliver's Travels",
-    author: "Jonathan Swift"
-
+    author: "Jonathan Swift",
+    isRead: true
 }
 let  myLibrary = [testBook1];
 displayLibrary(myLibrary);
 
-function Book(title, author, numberOfPages, readStatus){
+function Book(title, author, numberOfPages, isRead){
     this.title = title;
     this.author = author;
     this.numberOfPages = this.numberOfPages;
-    this.readStatus = readStatus;
+    this.readStatus = isRead;
 }
 
 
@@ -28,34 +28,48 @@ function displayLibrary(library){
         const bookEl = createBookElement(bookObj);
         //add book to library display
         console.log(bookEl);
+        // add identifier
+        bookEl.setAttribute('data-id', i);
         libraryContainer.appendChild(bookEl);
-
-        
     }
-    
-
 }
 function createBookElement(book){
     const bookShelf = document.querySelector('#bookShelf');
     const bookContainer = document.createElement('span');
-    // add identifier
-    bookContainer.setAttribute('data-id', i);
-    bookContainer.appendChild(document.createElement('h1'));
+    bookContainer.appendChild(document.createElement('p'));
     // add title
-    bookContainer.querySelector('h1').innerText = book.title;
+    bookContainer.querySelector('p').innerText = book.title;
     // create read status button
     const readStatusContainer = document.createElement('div');
-    readStatusContainer.appendChild(document.createElement('button')).classList.add('readStatus');
-    readStatusContainer.querySelector('button').addEventListener('click', toggleReadStatus);
-    
-    
-    // add read status button
-    const readStatus = bookContainer.appendChild(readStatusContainer);
-    
+    const readButton = readStatusContainer.appendChild(document.createElement('button'))
+    readButton.classList.add('readStatus');
+    readButton.innerText = 'Read';
+    if (book.isRead) readButton.classList.add('isRead');
+    readButton.addEventListener('click', toggleReadStatus);
     // add delete button
+    const delContainer = document.createElement('div');
+    const delButton = delContainer.appendChild(document.createElement('button'));
+    delButton.classList.add('delButton');
+    delButton.innerText = 'Delete';
+    delButton.addEventListener('click', deleteBook);
+    // add buttons
+    bookContainer.appendChild(readStatusContainer);
+    bookContainer.appendChild(delContainer);
     return bookContainer;
-
 }
-function toggleReadStatus(){
+function toggleReadStatus(eventData){
+    // check current status
+    const bookIndex = eventData.target.parentElement.parentElement.getAttribute('data-id');
+    const bookData = myLibrary[parseInt(bookIndex)];
+    // update state
+    if (bookData.isRead){
+        eventData.target.classList.remove('isRead');
+        bookData.isRead = false;
+    } else {
+        eventData.target.classList.add('isRead');
+        bookData.isRead = true;
+    }
+}
+function deleteBook(){
 
 }
